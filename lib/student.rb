b2f911ac8300342289c17c25a345a44620c4a5a5
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-05 17:21:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,5 +7,15 @@ class Student
     @id, @name, @grade = id, name, grade
   end
 
+  def create_table
+    sql = <<- SQL
+      CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+      SQL
+
+      DB[:conn].execute(sql)
 
 end
